802a6ab8d98268d82f5272d27384c45b1cd35cc8
tnoktkm <peskow.igor@gmail.com>
2021-03-07 21:33:03 +0400

    I hope It final commit for it task
    just commit

--- Task 1, matrix/main.cpp
+++ Task 1, matrix/main.cpp
@@ -2,6 +2,9 @@
 #include <iomanip> 
 #include <vector>
 
+using column_t = std::vector<double>;
+using matrix_t = std::vector<std::vector<double>>;
+
 class Matrix {
 
     /*Class Matrix for solve -> System Line Algebraical the Equations
@@ -12,13 +15,13 @@ class Matrix {
     */
 
 private:
-    std::vector<std::vector<double>> a;
-    std::vector<double> b;
+    matrix_t a;
+    column_t b;
 
 public:
     //constructor
-    Matrix(const std::vector<std::vector<double>> &a, const std::vector<double> &b) : a(a), b(b) {}
-        
+    Matrix(const matrix_t &a, const column_t &b) : a(a), b(b) {}
+
     //output matrix
     void getMatrix() {
         for (size_t i = 0; i < a.size(); ++i) {
@@ -28,6 +31,7 @@ public:
             std::cout << "|" << std::setprecision(4) << std::setw(8) << b[i];
             std::cout << std::endl;
         }
+        std::cout << std::endl;
     }
 
     /*
@@ -65,12 +69,12 @@ public:
     }
 
     //getting vector a
-    std::vector<std::vector<double>>& getA() {
+    matrix_t& getA() {
         return a;
     }
 
     //getting vector b
-    std::vector<double>& getB() {
+    column_t& getB() {
         return b;
     }
 
@@ -88,8 +92,8 @@ Matrix forwardRunGaus(Matrix &test) {
     return test;
 }
 
-std::vector<double> reverseGaus(Matrix test) {
-    std::vector<double> x(test.getA()[0].size());
+column_t reverseGaus(Matrix test) {
+    column_t x(test.getA()[0].size());
     for (int i = test.getA()[0].size() - 1; i >= 0; --i) {
         double sum = 0;
         for (size_t j = i + 1; j < test.getA()[0].size(); ++j) {
@@ -100,22 +104,48 @@ std::vector<double> reverseGaus(Matrix test) {
     return x;
 }
 
-void outputVector(std::vector<double> x) {
+void outputVector(column_t x) {
     for (size_t i = 0; i < x.size(); ++i) {
         std::cout << "x" << i + 1 << " =" << std::setw(8) << std::setprecision(4) << x[i] << std::endl;
     }
+    std::cout << std::endl;
+}
+
+void checkingAnswer(matrix_t a, column_t x) {
+    for (size_t i = 0; i < a.size(); ++i) {
+        double sumRow = 0; 
+        
+        for (size_t j = 0; j < a[0].size(); ++j) {
+            sumRow += a[i][j] * x[j];
+        }
+        std::cout << "b" << i + 1 << " =" << std::setw(8) << std::setprecision(4) << sumRow << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 
 
 
+
 int main() {
-    std::vector <std::vector<double>> a = { {1, 2, 4}, {4, 5, 4}, {3, 7, 3} };
-    std::vector<double>               b = { 5, 6, 4 };
+    std::vector <column_t> a = { {-0.512, -0.788, -0.495, -0.363, -0.793, 1.032},
+                                            { 0.570,  0.379,  1.054,  0.697,  0.269, 0.034},
+                                            {-0.187, -1.369,  0.119,  0.208, -1.105, 0.467},
+                                            { 0.863,  0.710, -0.877,  1.164, -0.867,-0.168},
+                                            { 1.101,  0.914, -0.976,  1.227, -0.580, 0.284},
+                                            {-0.443,  0.297,  1.234,  0.484,  0.849, 0.619}
+                                        };
+                                            
+    column_t               b = { 0.424, -0.005,  0.939, -0.589, -0.588, -0.433};
 
     Matrix test(a, b);
-
+    
+    test.getMatrix();
     forwardRunGaus(test).getMatrix();
-    outputVector(reverseGaus(test));
+    
+    column_t x = reverseGaus(test);
+    outputVector(x);
+    
+    checkingAnswer(a, x);
 
 }
